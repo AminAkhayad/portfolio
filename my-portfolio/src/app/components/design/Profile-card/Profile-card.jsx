ee3e80c4f94105788a17026d6457cd2d09735807
pgm-aminakha <amin.akhayad@student.arteveldehs.be>
2025-10-08 16:45:38 +0200

    Add social links to ProfileCard component; include GitHub and LinkedIn icons with hover effects

--- my-portfolio/src/app/components/design/Profile-card/Profile-card.jsx
+++ my-portfolio/src/app/components/design/Profile-card/Profile-card.jsx
@@ -1,14 +1,18 @@
 import './Profile-card.css';
 import profileIcon from '../../../../assets/amin.jpg'
 import { Link } from 'react-router-dom';
-
+import githubIcon from '../../../../assets/github.svg';
+import linkedinIcon from '../../../../assets/linkedIn.svg';
 const ProfileCard = () => {
     return (
         <div className="profile-card">
             <img src={profileIcon} alt="Profile" className="profile-pic" />
             <h2 className="name">Amin Akhayad</h2>
             <p className="bio">Een jonge ontwikkelaar met een passie voor webontwikkeling.</p>
-            
+            <ul className='social-links'>
+                <li><Link to="https://twitter.com/aminakhayad" target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="Twitter" /></Link></li>
+                <li><Link to="https://linkedin.com/in/aminakhayad" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="LinkedIn" /></Link></li>
+            </ul>
             
         </div>
     );
