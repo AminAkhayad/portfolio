4523c849100b4b010bf5b61cdefa2009fa8cb356
pgm-aminakha <amin.akhayad@student.arteveldehs.be>
2025-10-08 14:00:15 +0200

    Enhance Header component with icons and tooltips; update styles for improved user interaction and add custom cursor

--- my-portfolio/src/app/components/design/Header/Header.jsx
+++ my-portfolio/src/app/components/design/Header/Header.jsx
@@ -1,13 +1,16 @@
 import { Link, Outlet  } from "react-router-dom";
+import homeIcon from '../../../../assets/home.svg'
+import folderIcon from '../../../../assets/folder.svg'
+import aboutIcon from '../../../../assets/about.svg'
 import './Header.css'
 const Header = () => {
     return (
         <header>
             <nav>
                 <ul>
-                    <li><Link to="/"><span>Home</span></Link></li>
-                    <li><Link to="/about"><span>About</span></Link></li>
-                    <li><Link to="/projects"><span>Projects</span></Link></li>
+                    <li><Link to="/"><img src={homeIcon} alt="Home" /><span>Home</span></Link></li>
+                    <li><Link to="/about"><img src={aboutIcon} alt="About" /><span>About</span></Link></li>
+                    <li><Link to="/projects"><img src={folderIcon} alt="Projects" /><span>Projects</span></Link></li>
                 </ul>
             </nav>
         </header>
